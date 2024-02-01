7455d33059cf2c48df9e7ff31316b91672ccb44c
Siddharth Saha <siddharthsaha03@gmail.com>
2024-02-01 16:13:15 -0600

    retrieving links for langchain webcrawler

--- input.py
+++ input.py
@@ -1,4 +1,6 @@
 import http.client
+import json 
+
 
 conn = http.client.HTTPSConnection("google-news-api1.p.rapidapi.com")
 
@@ -7,11 +9,40 @@ headers = {
     'X-RapidAPI-Host': "google-news-api1.p.rapidapi.com"
 }
 
-query = "Delhi%20riots"
+file_path = "sample_dict.json"
+query = ""
 
-conn.request("GET", "/search?language=en&q=Elon%20Musk", headers=headers)
+#connect to API
+# conn.request("GET", "/search?language=en&q=Elon%20Musk", headers=headers)
+# res = conn.getresponse()
+# data = res.read() #json object
+# res_string = data.decode("utf-8")
 
-res = conn.getresponse()
-data = res.read()
+#store output in json
+# with open(file_path, 'w') as json_file:
+#     json.dump(res_string, json_file, indent=2) #store in json file
 
-print(data.decode("utf-8"))
+#load data as a string
+with open(file_path, 'r') as json_load:
+    loaded_string = (json.load(json_load)) #string that was dumped into json
+
+#convert to dictionary
+res_dict = (json.loads(loaded_string))
+print(res_dict)
+
+#extract a certain type of value from dictionary
+def extract_values(dictionary, searchterm):
+    result = []
+    for key, value in dictionary.items():
+        if key == searchterm:
+            result.append(value)
+        elif isinstance(value, dict):
+            result.extend(extract_values(value, searchterm))
+        elif isinstance(value, list):
+            for i in value:
+                result.extend(extract_values(i, searchterm))
+    return result
+output_values = extract_values(res_dict, "body")
+
+print(len(output_values))
+print(output_values[17])
