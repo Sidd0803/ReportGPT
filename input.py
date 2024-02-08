ffe43349f05a9eca15484b412dd7251258061a9d
Siddharth Saha <siddharthsaha03@gmail.com>
2024-02-08 10:54:29 -0600

    starting the chunking process

--- input.py
+++ input.py
@@ -1,6 +1,8 @@
 import http.client
 import json 
-
+import re
+# from langchain_community.document_loaders import WebBaseLoader #some bug
+# from langchain.document_loaders import WebBaseLoader
 
 conn = http.client.HTTPSConnection("google-news-api1.p.rapidapi.com")
 
@@ -28,7 +30,7 @@ with open(file_path, 'r') as json_load:
 
 #convert to dictionary
 res_dict = (json.loads(loaded_string))
-print(res_dict)
+#print(res_dict)
 
 #extract a certain type of value from dictionary
 def extract_values(dictionary, searchterm):
@@ -42,7 +44,25 @@ def extract_values(dictionary, searchterm):
             for i in value:
                 result.extend(extract_values(i, searchterm))
     return result
-output_values = extract_values(res_dict, "body")
 
-print(len(output_values))
-print(output_values[17])
+#can retrieve url/body using rapid api
+items = extract_values(res_dict, "body")
+print(len(items), " items retrieved!")
+article = items[17]
+
+#try loading content separately using webLoader
+# loader = WebBaseLoader(links[9])
+# docs = loader.load()
+# print(type(docs[0].page_content))
+# page_contents = docs[0].page_content
+trimmed_article = " ".join(article.split())
+re.sub(' +', ' ', article).strip()
+print(trimmed_article)
+
+#print(docs[0].page_content)
+
+#store final output in a text file (eventually transition to a proper data store)
+file_path = "article.txt"
+with open(file_path, "w") as file:
+    file.write(trimmed_article)
+print("Article has been written to the file:", file_path)
